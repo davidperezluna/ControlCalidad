04c213b4d41563e5e70e6737079be6fe84669084
daniielperez <daniel.perezluna94@gmail.com>
2016-12-13 10:31:00 -0500

    sync

--- CotrolCalidad/src/AppBundle/Form/ArchivoType.php
+++ CotrolCalidad/src/AppBundle/Form/ArchivoType.php
@@ -4,8 +4,9 @@ namespace AppBundle\Form;
 
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class ArchivoType extends AbstractType
 {
@@ -14,8 +15,31 @@ class ArchivoType extends AbstractType
      */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $builder->add('version')->add('descripcion')->add('estado')->add('urlDocumento', FileType::class)
-        ->add('urlDocumentoPdf', FileType::class)->add('proceso')->add('procedimiento')        ;
+        $builder
+        ->add('version', TextType::class,array(
+                "attr" =>array("class" => "form-control")
+            ))
+        ->add('descripcion', TextType::class,array(
+                "attr" =>array("class" => "form-control")
+            ))
+        ->add('urlDocumento', FileType::class,array(
+            "label" => "Documento Orijinal",
+            'attr' =>array("class" =>"file file-5", "data-preview-file-type"=>"any", "data-upload-url"=>"#"),
+            "data_class" => null
+          )) 
+        ->add('urlDocumentoPdf', FileType::class,array(
+            "label" => "Documento",
+            'attr' =>array("class" =>"file file-5", "data-preview-file-type"=>"any", "data-upload-url"=>"#"),
+            "data_class" => null
+          )) 
+
+        // ->add('proceso', TextType::class,array(
+        //         "attr" =>array("class" => " hidden")
+        //     ))
+        // ->add('procedimiento', TextType::class,array(
+        //         "attr" =>array("class" => " hidden")
+        //     ))
+        ;
     }
     
     /**
