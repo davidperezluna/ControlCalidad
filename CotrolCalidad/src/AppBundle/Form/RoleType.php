cee7c926167fb42993390bf859c3d80f266c37cc
daniielperez <daniel.perezluna94@gmail.com>
2016-12-14 11:26:19 -0500

    sync

---
+++ CotrolCalidad/src/AppBundle/Form/RoleType.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace AppBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class RoleType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+        ->add('nombre', TextType::class,array(
+                "attr" =>array("class" => "form-control") 
+            ))
+        ->add('cargo', TextType::class,array(
+                "attr" =>array("class" => "form-control") 
+            ));
+    }
+    
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'AppBundle\Entity\Role'
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'appbundle_role';
+    }
+
+
+}
