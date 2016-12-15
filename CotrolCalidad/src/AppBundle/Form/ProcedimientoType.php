2be10baf6716a4a0a2a4b5551d0040e82c2b4f61
davidperezluna <david.perez.udenar@gmail.com>
2016-12-15 15:42:07 -0500

    comit

--- CotrolCalidad/src/AppBundle/Form/ProcedimientoType.php
+++ CotrolCalidad/src/AppBundle/Form/ProcedimientoType.php
@@ -5,6 +5,7 @@ namespace AppBundle\Form;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -17,6 +18,10 @@ class ProcedimientoType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
+
+        ->add('codigo', NumberType::class,array(
+                "attr" =>array("class" => "form-control") 
+            ))
         ->add('nombre', TextType::class,array(
                 "attr" =>array("class" => "form-control") 
             ))
--- CotrolCalidad/src/AppBundle/Form/ProcesoType.php
+++ CotrolCalidad/src/AppBundle/Form/ProcesoType.php
@@ -5,6 +5,7 @@ namespace AppBundle\Form;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -17,6 +18,9 @@ class ProcesoType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
+        ->add('codigo', numberType::class,array(
+                "attr" =>array("class" => "form-control") 
+            ))
         ->add('nombre', TextType::class,array(
                 "attr" =>array("class" => "form-control") 
             ))
@@ -26,11 +30,6 @@ class ProcesoType extends AbstractType
         ->add('vigencia',TextType::class,array(
                 "label" => "Vigencia:",
                 "attr" =>array("class" => "form-control") 
-            ))
-        ->add('macroProceso', EntityType::class,array(
-                "class" => "AppBundle:MacroProceso",
-                "label" => "Macro Proceso:",
-                "attr" =>array("class" => "form-control hidden")
             ));
     }
     
